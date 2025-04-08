a3a304c1443d0e3fe9f654afce68ecec417cce53
lZ3R0l <galaxyeye.1999@gmail.com>
2025-04-08 22:05:34 +0200

    added "list movies"

--- main.js
+++ main.js
@@ -46,4 +46,39 @@ function addMovie() {
     }
 }
 
+function listMovies() {
+    if (movies.length === 0) {
+        alert("No movies in the list.");
+        return;
+    } else {
+        listChoice = prompt(`
+            List of Movies:\n
+            1) All Movies \n
+            2) Watched Movies \n
+            3) Unwatched Movies \n
+            `);
+        
+        switch (listChoice) {
+            case '1':
+                let movieList = "Movies:\n";
+                movies.forEach((movie, index) => {
+                movieList += `${index + 1}) ${movie.title} by ${movie.director} - ${movie.watched ? 'Watched' : 'Not Watched'}\n`;
+                });
+                alert(movieList);
+                break;
+            case '2':
+                const watchedMovies = movies.filter(movie => movie.watched).map(movie => `${movie.title} by ${movie.director}`).join('\n');
+                alert(watchedMovies.length > 0 ? watchedMovies : "No watched movies.");
+                break;
+            case '3':
+                const unwatchedMovies = movies.filter(movie => !movie.watched).map(movie => `${movie.title} by ${movie.director}`).join('\n');
+                alert(unwatchedMovies.length > 0 ? unwatchedMovies : "No unwatched movies.");
+                break;
+            default:
+                alert("Invalid choice.");
+        }
+    }
+}
+
+
 movies();
