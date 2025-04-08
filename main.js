bf15f71e0e73c784735903990d522ed956cbe648
lZ3R0l <galaxyeye.1999@gmail.com>
2025-04-08 22:04:11 +0200

    added "add movie"

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const movies = [
-    
+    { title: 'Inception', director: 'Christopher Nolan', watched: false },
+    { title: 'The Matrix', director: 'The Wachowskis', watched: false },
+    { title: 'Fast & Furious', director: 'Rob Cohen', watched: true },
 ]
 
 function movies() {
@@ -7,22 +9,22 @@ function movies() {
 
     do {
         choice = prompt(`
-            Welcome to the ATM! Enter your choice:\n
-            1) Add movies \n
-            2) List movies \n
-            3) Mark movie as watched \n
+            Movie Tracker:\n
+            1) Add Movies \n
+            2) List Movies \n
+            3) Mark Movie as Watched \n
             4) Exit
             `);
 
         switch (choice) {
             case '1':
-                
+                addMovie();
                 break;
             case '2':
-                
+                listMovies();
                 break;
             case '3':
-                
+                watchedMovie();
                 break;
             case '4':
                 alert("Goodbye!");
@@ -31,6 +33,17 @@ function movies() {
                 alert("Invalid choice.");
         }
     } while (choice !== '4');
+}
+
+function addMovie() {
+    const movieTitle = prompt("Enter movie title:");
+    const movieDirector = prompt("Enter movie director:");
+    if (movieTitle && movieDirector) {
+        movies.push({ title: movieTitle, director: movieDirector, watched: false });
+        alert(`Movie "${title}" by ${director} has been added.`);
+    } else {
+        alert("Invalid input. Both title and director are required.");
+    }
 }
 
 movies();
